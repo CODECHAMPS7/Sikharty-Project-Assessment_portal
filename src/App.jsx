740f44a563d4e0087b81595e7abcb68de70504f5
arnav <arnabdas2753@gmail.com>
2024-09-17 03:11:00 +0530

    Fornted UI task: init1

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,37 @@
-export default function App() {
+import { BrowserRouter as Router, Routes, Route, Outlet } from 'react-router-dom';
+
+import Home from './components/Home';
+import Body from './components/Body';
+import Login from './Pages/Login';
+import Signup from './Pages/SIgnup';  // Fix casing for Signup
+import Dashboard from './Pages/Dashboard';
+import Profile from './Pages/profile';
+
+const Layout = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+    <>
+      <Home />
+      <Outlet /> {/* This renders the specific route component */}
+    </>
+  );
 }
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+      
+        <Route path="/" element={<Layout />}>
+         
+          <Route path="body" element={<Body />} />
+          <Route path="login" element={<Login />} />
+          <Route path="signup" element={<Signup />} /> 
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="profile" element={<Profile />} />
+        </Route>
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
