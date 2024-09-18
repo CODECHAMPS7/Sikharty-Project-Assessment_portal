b884cec69127b7a001da85c52109e1a7d360d25b
DC104 <debosmitachakraborty11@gmail.com>
2024-09-18 22:19:47 +0530

    Updated Announcements, Home, and Links components

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,10 +1,9 @@
 const Body = () => {
   return (
-    <div className="p-5 max-w-2xl mx-auto text-center">
-      <h1 className="text-4xl font-bold my-5">Body</h1>
-      <p className="text-lg">
-       Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo id sunt quam unde velit officia enim ad iusto nulla, fugit odit saepe consequuntur tenetur beatae, laboriosam, dolore sint? Non, debitis!
-      </p>
+    <div>
+
+      <h1>Body</h1>
+      
     </div>
   );
 }
